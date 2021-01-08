703f6b1794d41efa54f507d94b3fc0983c716514
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2021-01-08 20:26:45 +0000

    gh pages deploy (#5)

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <Helmet>
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width,initial-scale=1.0'></meta>
-        <title>Tech Donut for life #billing team</title>
+        <title>Tech donut 🍩 🍩 🍩 </title>
       </Helmet>
       <Container/>
     </>
