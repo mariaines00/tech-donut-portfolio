a44e651d6c9cfad1e5961cf9229439c0797e2878
Ana Fig <ana.mimoso@talkdesk.com>
2021-01-11 16:52:58 +0000

    Add Portfolio Landing Pages

---
+++ src/layout/LandingHome/LandingHome.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import donut from '../../assets/images/donut.png'
+
+class LandingHome extends Component {
+  render() {
+    return (
+      <div className='landing'>
+        <div className='landing__title'>
+          Tech Donut <img alt='Donut' src={donut} height='100px' />
+        </div>
+        <div className='landing__subtitle'>Move your ass and bring us a donut</div>
+        <div className='landing__user-info'>
+          Fullstack Developers
+          <p className='landing__user-info--paragraph'>Frontend | Backend enthusiasts</p>
+        </div>
+        {/* FAZERRRR <ProjectsButton /> */}
+      </div>
+    )
+  }
+}
+export default LandingHome
