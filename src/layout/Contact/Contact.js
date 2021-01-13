bbfedfe07eddbdc9d06d84092ff5d2c4a8dd6e05
francisco-carrola <francisco.carrola@talkdesk.com>
2021-01-13 16:46:50 +0000

    add button

--- src/layout/Contact/Contact.js
+++ src/layout/Contact/Contact.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
+import Button from '../../components/Button'
 
 class Contact extends Component {
   render() {
-    return <div className='contact'>Contact</div>
+    return (<div className='contact'> Contact
+      <Button text={"Ola"}/>
+    </div>
+    )
+
   }
 }
 export default Contact
