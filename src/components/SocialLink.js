85be5ba40845dac08157682ec66ceab6752c32c6
francisco-carrola <francisco.carrola@talkdesk.com>
2021-01-14 15:56:59 +0000

    add social links and icons

---
+++ src/components/SocialLink.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {FaGithub,FaLinkedin} from 'react-icons/fa'
+
+const SocialLink = (props) => {
+  return (
+      <div>
+          <a className='social' href={props.linkedin} target="_blank" rel="noreferrer">
+            <FaLinkedin size={30}/>
+          </a>
+          <a className='social' href={props.github} target="_blank" rel="noreferrer">
+            <FaGithub size={30}/>
+          </a>
+      </div>
+  )
+}
+
+export default SocialLink
--- src/layout/About/About.js
+++ src/layout/About/About.js
@@ -2,27 +2,34 @@ import React, { Component } from 'react'
 import Ana from '../../assets/images/Ana.png'
 import Francisco from '../../assets/images/Francisco.png'
 import Maria from '../../assets/images/Maria.png'
-import  {Person, PersonReverse}from '../../components/Person'
-
-
+import  {Person, PersonReverse} from '../../components/Person'
 class About extends Component {
   render() {
+    const anaSocial = ['https://www.linkedin.com/in/ana-mimoso/', 'https://github.com/anafigm']
+    const franciscoSocial = ['https://www.linkedin.com/in/francisco-carrola-540694171/', 'https://github.com/francisco-carrola']
+    const mariaSocial = ['https://www.linkedin.com/in/maria-ines-serra/', 'https://github.com/mariaines00']
     return (
       <div className='about'>
         <Person
-          description={'Cat Lover and coffee enthusiast | Master Gamer | Full Stack Developer'}
           title={'Ana Fig'}
+          subtitle={'Cat Lover and coffee Enthusiast | Master Gamer | Full Stack Developer'}
+          description={'Random String trying to explain something random'}
           image={Ana}
+          links={anaSocial}
         />
         <PersonReverse
-          description={'Dog Lover and Tea enthusiast | Wannabe Chef | Backend Developer'}
           title={'Francisco Carrola'}
+          subtitle={'Wannabe Chef | Earl Grey fanboy | Backend Developer'}
+          description={'Random String trying to explain something random'}
           image={Francisco}
+          links={franciscoSocial}
         />
         <Person
-          description={'Plants caretaker | Pizza demolisher | Backend Developer'}
           title={'Maria Inês Serra'}
+          subtitle={'Zen Gardener | Pizza Critic | Backend Developer'}
+          description={'Random String trying to explain something random'}
           image={Maria}
+          links={mariaSocial}
         />
       </div>
     )
