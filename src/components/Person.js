ebe234075275c9f167ed1daead4f5eea68d889e6
francisco-carrola <francisco.carrola@talkdesk.com>
2021-01-14 15:56:59 +0000

    add social links and icons

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import SocialLink from './SocialLink'
 
 export const Person = (props) => {
   return (
@@ -6,7 +7,9 @@ export const Person = (props) => {
       <img className='about__box--image' alt='' src={props.image} width='150' height='200' />
       <div>
         <div className='about__box--title'>{props.title}</div>
+        <div className='about__box--subtitle'>{props.subtitle}</div>
         <div className='about__box--description'>{props.description}</div>
+        <SocialLink linkedin={props.links[0]} github={props.links[1]}></SocialLink>
       </div>
     </div>
   )
@@ -18,9 +21,10 @@ export const PersonReverse = (props) => {
         <img className='about__box--reverse--image' alt='' src={props.image} width='150' height='200' />
         <div>
           <div className='about__box--reverse--title'>{props.title}</div>
-          <div className='about__box--reverse--description'>{props.description}</div>
+          <div className='about__box--reverse--subtitle'>{props.subtitle}</div>
+          <div className='about__box--description'>{props.description}</div>
+          <SocialLink linkedin={props.links[0]} github={props.links[1]}></SocialLink>
         </div>
       </div>
     )
   }
-
