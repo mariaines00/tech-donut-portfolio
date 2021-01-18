1a275d84e77bbec4b10ac0c7e7a4f45c9bb6cf60
Ana Fig <ana.mimoso@talkdesk.com>
2021-01-18 23:45:41 +0000

    Add github graphic

--- src/components/SocialLink.js
+++ src/components/SocialLink.js
@@ -1,16 +1,21 @@
 import React from 'react'
-import {FaGithub,FaLinkedin} from 'react-icons/fa'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
 
 const SocialLink = (props) => {
   return (
-      <div>
-          <a className='social' href={props.linkedin} target="_blank" rel="noreferrer">
-            <FaLinkedin size={30}/>
-          </a>
-          <a className='social' href={props.github} target="_blank" rel="noreferrer">
-            <FaGithub size={30}/>
-          </a>
-      </div>
+    <div className='social'>
+      <a className='social--color' href={props.linkedin} target='_blank' rel='noreferrer'>
+        <FaLinkedin size={30} />
+      </a>
+      <a
+        className='social--color social--padding'
+        href={props.github}
+        target='_blank'
+        rel='noreferrer'>
+        <FaGithub size={30} />
+      </a>
+      {props.showGithubGraphic}
+    </div>
   )
 }
 
