0699e227d70c78539f758df324449d6bf90ffba6
francisco-carrola <francisco.carrola@talkdesk.com>
2021-01-18 16:43:05 +0000

    copyright section

---
+++ src/layout/Copyright/Copyright.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Copyright extends Component {
+  render() {
+    return (
+      <div className='copyright'>
+        ® TechDonut brought to you by Ana Fig, Francisco Carrola, Maria Inês Serra
+      </div>
+    )
+  }
+}
+export default Copyright
