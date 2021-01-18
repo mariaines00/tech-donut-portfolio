0776cd1d4c55da71d697bbe0bb7aa317347ef93c
Maria Inês Serra <maria.serra@talkdesk.com>
2021-01-18 19:46:42 +0000

    fix social link spacings

--- src/components/SocialLink.js
+++ src/components/SocialLink.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import {FaGithub,FaLinkedin} from 'react-icons/fa'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
 
 const SocialLink = (props) => {
   return (
-      <div>
-          <a className='social' href={props.linkedin} target="_blank" rel="noreferrer">
-            <FaLinkedin size={30}/>
-          </a>
-          <a className='social' href={props.github} target="_blank" rel="noreferrer">
-            <FaGithub size={30}/>
-          </a>
-      </div>
+    <div>
+      <a className='social' href={props.linkedin} target="_blank" rel="noreferrer">
+        <FaLinkedin size={30}/>
+      </a>
+      <a className='social' href={props.github} target="_blank" rel="noreferrer">
+        <FaGithub size={30}/>
+      </a>
+    </div>
   )
 }
 
