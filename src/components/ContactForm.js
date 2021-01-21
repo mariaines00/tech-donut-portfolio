f03b75460065a92a36d2ecd0455a44b9d2a86172
Ana Fig <ana.mimoso@talkdesk.com>
2021-01-21 13:16:30 +0000

    Improve form of sending email

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,21 +1,72 @@
 import React, { Component } from 'react'
+import { OWNERS_EMAILS, EMAIL_SUBJECT } from '../constants/ui.constants'
 
 class ContactForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      email: '',
+      message: '',
+    }
+    this.onEmailChange = this.onEmailChange.bind(this)
+  }
+
+  onEmailChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    })
+  }
+
   render() {
+    const { name, email, message } = this.state
+
+    const mailTo =
+      'mailto:' +
+      OWNERS_EMAILS +
+      '?cc=' +
+      email +
+      '&subject=' +
+      name +
+      EMAIL_SUBJECT +
+      '&body=' +
+      message
+
     return (
       <div>
         <div className='contact__title'>GIVE US FEEDBACK</div>
-        <form action='mailto:maria.talkdesk@talkdesk.com' method='GET' className='contact__form'>
+        <form className='contact__form'>
           <label className='contact__form__label'>Name</label>
-          <input className='contact__form__input' name='subject' type='text' />
+          <input
+            className='contact__form__input'
+            name='name'
+            type='text'
+            value={this.state.name}
+            onChange={this.onEmailChange}
+          />
           <br />
           <label className='contact__form__label'>Email</label>
-          <input className='contact__form__input' name='cc' type='email' />
+          <input
+            className='contact__form__input'
+            name='email'
+            type='email'
+            value={this.state.email}
+            onChange={this.onEmailChange}
+          />
           <br />
           <label className='contact__form__label'>Message</label>
-          <textarea className='contact__form__input--textarea' name='body'></textarea>
+          <textarea
+            className='contact__form__input--textarea'
+            name='message'
+            value={this.state.message}
+            onChange={this.onEmailChange}
+            type='text'
+            rows='7'
+            cols='60'></textarea>
           <br />
-          <input className='contact__form__input--submit' type='submit' value='Send' />
+          <a className='contact__form__input--submit' href={mailTo}>
+            Send
+          </a>
         </form>
       </div>
     )
